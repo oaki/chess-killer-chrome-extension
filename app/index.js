0b3901d22297240361839b0691f8fab1f22b4059
pavolbincik <pavolbinck@gmail.com>
2018-08-18 13:18:10 +0200

    added some book from internet

--- app/index.js
+++ app/index.js
@@ -1,95 +1,211 @@
-import * as $ from 'jquery';
+import * as $ from "jquery";
 import * as io from "socket.io-client";
 const Chess = require('chess.js');
 // const HOST = 'https://chess-analysis.ddns.net:5000/';
 const HOST = 'https://localhost:5000/';
-const $banner = $('<div style="position: absolute;top:0;left:0;width:100%;height:30px;background-color: beige;"></div>');
+const MULTI_VP = 3;
+const $banner = $('<div style="position: absolute;top:0;left:0;background-color: beige;"></div>');
 
-
+console.log('Loading...');
 const socket = io.connect(HOST);
 
 const showResult = (text) => {
-    $banner.html(text);
+  $banner.html(text);
 };
 
 const getMoves = () => {
-    return $('.moves').find('move');
+  return $('.moves').find('move');
 };
 
+
+function pairValues(name, str) {
+  const tmp = str.split(' ');
+
+  const namePosition = tmp.indexOf(name);
+
+  // for(let i = 0; i< tmp.length; i+=2){
+  //     result[tmp[i]] = tmp[i+1];
+  // }
+  if (namePosition === -1) {
+    return false;
+  }
+
+  if (name === 'pv') {
+    const tmpArr = tmp.splice(namePosition + 1);
+    return tmpArr.join(" ");
+  }
+  return tmp[namePosition + 1];
+
+}
+
+function parseLine(lineStr) {
+  const mate = pairValues('mate', lineStr);
+  const score = parseFloat(pairValues('cp', lineStr)) / 100;
+  const depth = pairValues('depth', lineStr);
+  const pv = pairValues('pv', lineStr);
+  const multipv = pairValues('multipv', lineStr);
+
+  return {
+    mate,
+    score,
+    depth,
+    pv,
+    multipv
+  }
+}
+
+
 const parseResult = (result, fen) => {
-    const tmp = result.split(' pv');
-    const l = tmp.length;
-    const bestconstiantLine = tmp[l - 2];
 
-    if (bestconstiantLine) {
-        const bestconstiantWithDepthArr = bestconstiantLine.split('info');
-        const bestconstiant = bestconstiantWithDepthArr[0];
-        const moves = bestconstiant.split(' ');
-        const chessNewconstiant = new Chess(fen);
+  if (result.indexOf('info') === -1) {
+    return false;
+  }
+  let lines = result.split('\n');
+  lines = lines.filter((line) => line.indexOf('info') !== -1 && line.indexOf('pv') !== -1);
+  if (lines.length < 1) {
+    return false;
+  }
 
-        $.each(moves, (index, move) => {
-            if (move !== '') {
-                const from = move.substr(0, 2);
-                const to = move.substr(2, 4);
+  let output = [];
 
-                chessNewconstiant.move({
-                    from,
-                    to
-                });
-            }
+  lines.forEach((line) => {
+    const r = parseLine(line);
+    output[parseInt(r.multipv) - 1] = r;
+  });
 
+  return output;
+}
+
+
+const parseResultOld = (result, fen) => {
+
+  if (result.indexOf('info') === -1) {
+    return false;
+  }
+  let lines = result.split('\n');
+  lines = lines.filter((line) => result.indexOf('info') !== -1);
+  if (lines.length < 1) {
+    return false;
+  }
+
+
+  lines.forEach((line) => {
+    const r = parseLine(line);
+    console.log(r);
+  });
+
+  const tmp = result.split(' pv ');
+  if (tmp.length < 2) {
+    return false;
+  }
+  const l = tmp.length;
+  const bestconstiantLine = tmp[l - 2];
+
+  if (bestconstiantLine) {
+    const bestconstiantWithDepthArr = bestconstiantLine.split('info ');
+    const bestconstiant = bestconstiantWithDepthArr[0];
+    const info = bestconstiantWithDepthArr[1];
+    const mate = pairValues('mate', info);
+    const score = parseFloat(pairValues('cp', info)) / 100;
+    const depth = pairValues('depth', info);
+    const moves = bestconstiant.split(' ');
+    const chessNewconstiant = new Chess(fen);
+
+    $.each(moves, (index, move) => {
+      if (move !== '') {
+        const from = move.substr(0, 2);
+        const to = move.substr(2, 4);
+
+        chessNewconstiant.move({
+          from,
+          to
         });
+      }
 
-        const fullPGN = chessNewconstiant.pgn({newline_char: "__SEP__"});
-        const notation = fullPGN.split("__SEP__");
-        showResult(notation[3]);
+    });
+
+    const fullPGN = chessNewconstiant.pgn({newline_char: "__SEP__"});
+    const notation = fullPGN.split("__SEP__");
+
+    let result = '';
+    if (mate) {
+      result += `mate: ${mate} `;
+    } else {
+      result += `score: ${score} `;
     }
+    result += `${notation[3]}, depth: ${depth}`;
+    showResult(result);
+  }
 }
 
 socket.on('on_result', (obj) => {
-    parseResult(obj.data, obj.fen);
+  console.log(obj.fen);
+  console.log(obj.data);
+  const result = parseResult(obj.data, obj.fen);
+
+  if (result) {
+
+    let str = '';
+    result.forEach((multipv, index) => {
+
+      if (multipv.mate) {
+        str += `<b style="display: inline-block; min-width:40px;"> Mate: ${multipv.mate} move, </b>`
+      } else {
+        str += `<b style="display: inline-block; min-width:40px;">${multipv.score}, </b>`
+      }
+
+      // str += ` ${index + 1}. `;
+      str += ` ${multipv.pv}, depth:${multipv.depth}  <br/>`;
+
+    });
+    showResult(str);
+  }
+
 })
 
 
 try {
-    $('body').append($banner);
+  $('body').append($banner);
 
-    const init = () => {
-        const chess = new Chess();
+  const init = () => {
+    const chess = new Chess();
 
-        getMoves().each((index, el) => {
-            let move = el.innerText;
-            move = move.replace('х', 'x');
-            chess.move(move);
-        });
+    getMoves().each((index, el) => {
+      let move = el.innerText;
+      move = move.replace('х', 'x');
+      chess.move(move);
+    });
 
-        let fen = chess.fen();
+    let fen = chess.fen();
 
-        console.log(['-----------------------']);
-        console.log(['-------Init----------']);
-        console.log(['-----------------------']);
-        console.log(chess.ascii());
-        console.log(chess.pgn());
+    console.log(['-----------------------']);
+    console.log(['-------Init----------']);
+    console.log(['-----------------------']);
+    console.log(chess.ascii());
+    console.log(chess.pgn());
 
-        socket.emit('new_move', {FEN: fen});
-    };
+    socket.emit('new_move', {FEN: fen});
+  };
 
 
+  setTimeout(()=>{
     const target = document.querySelector('.moves');
 
     if (target) {
-        // create an observer instance
-        const observer = new MutationObserver(function (mutations) {
-            init();
-        });
+      // create an observer instance
+      const observer = new MutationObserver(function (mutations) {
+        init();
+      });
 
-        // configuration of the observer:
-        const config = {attributes: false, childList: true, characterData: false}
+      // configuration of the observer:
+      const config = {attributes: false, childList: true, characterData: false}
 
-        // pass in the target node, as well as the observer options
-        observer.observe(target, config);
+      // pass in the target node, as well as the observer options
+      observer.observe(target, config);
     }
+  },2000)
+
 
 } catch (err) {
-    console.log(err);
+  console.log(err);
 }
--- server/server.js
+++ server/server.js
@@ -2,82 +2,115 @@ const app = require('express')();
 const https = require('https');
 const fs = require('fs');
 const PORT = 5000;
-const DELAY = 5000;
+// const DELAY = 5000;
+const DELAY = 'infinity';
+const MULTI_VP = 3;
+const DEPTH = 25;
 // const STOCKFISH_PATH = '/usr/games/stockfish';
 const STOCKFISH_PATH = __dirname + '/stockfish';
 const spawn = require('child_process').spawn;
 
 
-class EngineConnect {
-    constructor(id, cmd, args, callback) {
-        this.id = id;
-        this.cmd = cmd;
-        this.args = args;
-        this.child = spawn(this.cmd, this.args);
-        this.fen = '';
+class EngineInterface {
 
-        let result = '';
+  constructor(id, cmd) {
+    this.id = id;
+    this.cmd = cmd;
+    this.child = spawn(this.cmd, []);
+    this.fen = '';
 
-        this.child.stdout.on('data', function (buffer) {
-            this.stdout += buffer.toString();
-            result = this.stdout;
-        });
+    // let result = '';
+    //
+    // this.child.stdout.on('data', function (buffer) {
+    //   this.stdout += buffer.toString();
+    //   result = this.stdout;
+    // });
 
-        this.child.stdout.on('end', function () {
-            callback(this.stdout);
-            console.log('on->end ID:', id);
-        });
+    // this.child.stdout.on('end', function () {
+    //   callback(this.stdout);
+    //   console.log('on->end ID:', id);
+    // });
 
-        let previousResult = '';
-        this.loop = setInterval(() => {
-            if(previousResult !== result){
-                console.log('loop ID:', id, result);
-                callback(result);
-                previousResult = result;
-            }
-        }, 1000);
-    }
+    // let previousResult = '';
+    // this.loop = setInterval(() => {
+    //   if (previousResult !== result) {
+    //     console.log('loop ID:', id, result);
+    //     callback(result);
+    //     previousResult = result;
+    //   }
+    // }, 1000);
+  }
 
-    findBestMove(fen, delay) {
-        this.fen = fen;
-        this.child.stdin.write("position fen " + fen + "\ngo movetime " + delay + "\n");
-    }
+  on(handler, callback) {
+    this.child.stdout.on(handler, callback);
+  }
+
+  findBestMove(fen, delay) {
+    this.fen = fen;
+    this.send(`position fen ${fen}`);
+    this.send(`go movetime ${delay}`);
+  }
+
+  send(cmd) {
+    console.log('command: ' + cmd);
+    this.child.stdin.write(cmd + "\n");
+  }
 }
 
 
 const httpsOptions = {
-    key: fs.readFileSync(__dirname + '/key.pem'),
-    cert: fs.readFileSync(__dirname + '/cert.pem')
+  key: fs.readFileSync(__dirname + '/key.pem'),
+  cert: fs.readFileSync(__dirname + '/cert.pem')
 };
 
 const server = https.createServer(httpsOptions, app);
 
 server.listen(PORT, () => {
-    console.log('Server running at ' + PORT);
+  console.log('Server running at ' + PORT);
 });
 
 const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-
 io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
+  console.log('a user connected', socket.id);
 
-    let engine = new EngineConnect(socket.id, STOCKFISH_PATH, [], (bestmove) => {
-        console.log('bestmove', bestmove);
-        socket.emit('on_result', {
-            fen: engine.fen, data: bestmove
-        });
-    });
+  let engine = new EngineInterface(socket.id, STOCKFISH_PATH);
+  engine.send('uci');
+  engine.send('eval');
+  engine.send('isready');
+  engine.send('ucinewgame');
+  engine.send(`setoption name multipv value ${MULTI_VP}`);
+  engine.send('setoption name OwnBook value true');
+  // engine.send('go');
 
-    socket.on('new_move', (data) => {
-        engine.findBestMove(data.FEN, 'infinity');
-    });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected', socket.id);
+  engine.on('data', function (buffer) {
+
+    const bestmove = buffer.toString();
+    socket.emit('on_result', {
+      fen: engine.fen, data: bestmove
     });
+  });
+
+  engine.on('end', function (bestmove) {
+    // callback(this.stdout);
+
+    // console.log('bestmove', bestmove);
+    // socket.emit('on_result', {
+    //   fen: engine.fen, data: bestmove
+    // });
+  });
+
+  socket.on('new_move', (data) => {
+
+    engine.findBestMove(data.FEN, DELAY);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected', socket.id);
+  });
 });
